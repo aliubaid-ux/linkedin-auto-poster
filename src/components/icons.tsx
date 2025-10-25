b3e4c0141b80a6e3167ee8d8544a9a3ae8fbf06a
Ali Ubaid <ali.ubaid926@gmail.com>
2025-10-25 20:42:30 +0000

    fix: Add Logo component and move favicon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -20,3 +20,25 @@ export function LinkedInIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function Logo(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10 21h7a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-7a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2z" />
+      <path d="M10 10h4" />
+      <path d="M10 14h4" />
+      <path d="M10 6h4" />
+    </svg>
+  );
+}
